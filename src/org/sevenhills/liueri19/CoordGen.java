474a8a9f1920ba68dcd6dcad0a37fdbf01995130
liueri19 <liueri19@7hills.org>
2016-12-14 14:33:57 -0500

    parse input;

--- src/org/sevenhills/liueri19/CoordGen.java
+++ src/org/sevenhills/liueri19/CoordGen.java
@@ -6,13 +6,14 @@ public class CoordGen {
 
 	public static void main(String[] args) {
 		Random random = new Random();
+		int cities = 12;
 		
 		System.out.print("[ ");
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < cities; i++) {
 			int x = random.nextInt(100);
 			int y = random.nextInt(100);
 			System.out.printf("[ City_%d, %d, %d ]", i, x, y);
-			if (i != 9)
+			if (i != cities-1)
 				System.out.print(", ");
 		}
 		System.out.print(" ]");
